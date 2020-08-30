7b5447a6393aa93d33522ef609017fb737e674f2
Waseem Ullah <wasim_ullah@hotmail.com>
2020-08-30 11:46:11 +0500

    Upload personal portfolio project

---
+++ portfolio_project/personal_portfolio/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project
+
+
+# Create your views here.
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'personal_portfolio/index.html', {'projects':projects})
